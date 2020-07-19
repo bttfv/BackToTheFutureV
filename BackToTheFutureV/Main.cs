018b59e0d11c6f68aab816312fbdb275fb1449cf
Luca Di Giovanni <lucadigiovanni@outlook.it>
2020-07-19 23:05:01 +0200

    Enabled TrainMission

--- BackToTheFutureV/Main.cs
+++ BackToTheFutureV/Main.cs
@@ -107,6 +107,9 @@ namespace BackToTheFutureV
             ModMenuHandler.KeyDown(e);
             DeloreanHandler.KeyPressed(e.KeyCode);
             RCManager.KeyPress(e.KeyCode);
+
+            if (e.KeyCode == Keys.L)
+                MissionHandler.TrainMission.Start();
         }
 
         private unsafe void Main_Tick(object sender, EventArgs e)
