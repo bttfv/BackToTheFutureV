7a72b4ff2cf1149c679bbf508abfc978115193c6
nomakewan <nomakewan@users.noreply.github.com>
2022-10-08 17:37:35 -0700

    Sound and code updates
    Added missing sounds, removed unnecessary sounds, fixed typos related to sounds, made it so plutonium can be purchased from 1986 through 2014

--- BackToTheFutureV/Menu/GarageMenu.cs
+++ BackToTheFutureV/Menu/GarageMenu.cs
@@ -225,7 +225,7 @@ namespace BackToTheFutureV
             repairEngine.Enabled = active && FusionUtils.CurrentTime.Year >= 1912 && (CurrentTimeMachine.Vehicle.EngineHealth <= 0 && CurrentTimeMachine.Mods.Wheels.Burst);
             washCar.Enabled = active && CurrentTimeMachine.Vehicle.DirtLevel > 0;
 
-            buyPlutonium.Enabled = InternalInventory.Current.Plutonium < 5 && FusionUtils.CurrentTime.Year == 1985;
+            buyPlutonium.Enabled = InternalInventory.Current.Plutonium < 5 && FusionUtils.CurrentTime.Year > 1985 && FusionUtils.CurrentTime.Year < 2015;
             buyPlutonium.AltTitle = $"{InternalInventory.Current.Plutonium}/5";
 
             customMenu.Enabled = active && !CurrentTimeMachine.Constants.FullDamaged;
