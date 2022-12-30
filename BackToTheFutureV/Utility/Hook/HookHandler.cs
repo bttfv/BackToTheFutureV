baf0d49129de584aa1f823410d92e15f2329b071
bttf92 <lucadigiovanni@outlook.it>
2022-12-30 01:37:41 +0100

    Update HookHandler.cs

--- BackToTheFutureV/Utility/Hook/HookHandler.cs
+++ BackToTheFutureV/Utility/Hook/HookHandler.cs
@@ -25,6 +25,19 @@ namespace BackToTheFutureV
 
         private static CSpecialFlightHandlingData m_CustomFlightData = new CSpecialFlightHandlingData();
 
+        delegate long GetSubHandlingByTypeDelegate(long inst, int type);
+        static GetSubHandlingByTypeDelegate GetSubHandlingByType_Original;
+
+        unsafe static long GetSubHandlingByType_Hook(long inst, int type)
+        {
+            //if (type == 10) // rage::par::SUB_HANDLING_SPECIAL_FLIGHT
+            //{
+            //    fixed (CSpecialFlightHandlingData* i = &m_CustomFlightData) { return (long)i; }
+            //}
+
+            return GetSubHandlingByType_Original(inst, type);
+        }
+        
         public static void Setup()
         {
             deluxo_1 = Game.FindPattern("48 8B C4 48 89 58 08 48 89 68 10 48 89 70 18 48 89 78 20 41 56 48 83 EC 40 0F 29 70 E8 48 8B 01 4D 8B F1 0F 28 F2 8B EA 48 8B F9 FF 50 ?? 33 DB 48 85 C0 74 18 48 8B 48 68 48 85 C9 74 1E 48 39 58 78 74 18 48 8B 89 78 01 00 00 EB 12 48 8B 4F 50 48 85 C9 74 06 48 8B 49 28 EB 03 48 8B CB 48 63 C5 48 8D 34 80 48 8B 01 48 C1 E6 04 48 03 70 20 0F 84 F1");
@@ -38,6 +51,8 @@ namespace BackToTheFutureV
             {
                 handling_GetSubHandling = handling_GetSubHandling + 4 + *(int*)handling_GetSubHandling;
             }            
+
+            Main.Log(handling_GetSubHandling.ToString());
 
             // From handling.meta
             m_CustomFlightData.mode = 1;
@@ -76,6 +91,7 @@ namespace BackToTheFutureV
             Engine.CreateHook(deluxo_1, new DeluxoNullsubDelegate1(DeluxoNullsub1));
             Engine.CreateHook(deluxo_2, new DeluxoNullsubDelegate2(DeluxoNullsub2));
             //Engine.CreateHook(deluxo_3, new DeluxoNullsubDelegate3(DeluxoNullsub3));
+            Engine.CreateHook(handling_GetSubHandling, new GetSubHandlingByTypeDelegate(GetSubHandlingByType_Hook));
 
             Engine.EnableHooks();
         }
